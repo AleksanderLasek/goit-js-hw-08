6b0d1b0bfdde2a5ef91bd2044912b8e2fee62b40
Aleksander Lasek <aleksanderlasek01@gmail.com>
2022-04-07 19:05:40 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,42 @@
+const _ = require('lodash');
 
+const $iframe = document.querySelector('iframe');
+const player = new Vimeo.Player($iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
+
+player
+  .setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
+
+const timeCounter = function (event) {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+};
+
+player.on('timeupdate', _.throttle(timeCounter, 500));
