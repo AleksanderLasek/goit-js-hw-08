9dabdfefd22a9104cb443627abcd644bcf75ef38
Aleksander Lasek <aleksanderlasek01@gmail.com>
2022-04-07 16:36:49 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
+// Opisany w dokumentacji
+import SimpleLightbox from 'simplelightbox';
+// Dodatkowy import stylów
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const $gallery = document.querySelector('.gallery');
+
+for (let i = 0; i < galleryItems.length; i++) {
+  $gallery.insertAdjacentHTML('beforeend', `<a></a>`);
+  let $a = $gallery.lastChild;
+  $a.classList.add('gallery__item');
+  $a.href = galleryItems[i].original;
+}
+
+const $a = $gallery.querySelectorAll('a');
+let counter = 0;
+
+for (const a of $a) {
+  a.insertAdjacentHTML('beforeend', '<img/>');
+  let img = a.lastChild;
+  img.classList.add('gallery__image');
+  img.src = `${galleryItems[counter].preview}`;
+  img.alt = `${galleryItems[counter].description}`;
+  a.addEventListener('click', event => {
+    event.preventDefault();
+  });
+  counter++;
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
